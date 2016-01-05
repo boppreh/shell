9cfc96dfb0740e646e88e366b4350991819667d4
BoppreH <boppreh@gmail.com>
2016-01-05 11:41:19 -0200

    Add download button

--- shell.py
+++ shell.py
@@ -7,18 +7,28 @@ if __name__ == '__main__':
 
 	app = flask.Flask(__name__)
 	events_queue = Queue()
+	blocks = {}
 
 	@app.route('/')
 	def root():
 		return flask.send_file('index.html')
 
+	@app.route('/outputs/<id>')
+	def get_output(id):
+		return blocks[int(id)]
+
 	@app.route('/run', methods=['POST'])
 	def run():
 		try:
-			parts = json.loads(flask.request.form['data'])
-			first, *rest = filter(len, parts)
-			return subprocess.check_output(first.split() + rest, stderr=subprocess.STDOUT)
+			data = json.loads(flask.request.form['data'])
+			first, *rest = filter(len, data['parts'])
+			output = subprocess.check_output(first.split() + rest, stderr=subprocess.STDOUT)
+			blocks[data['id']] = output
+			try:
+				return output.decode('utf-8')
+			except UnicodeError:
+				return '{} bytes'.format(len(output))
 		except Exception as e:
-			return str(e)
+			return 'Shell error: {}'.format(e)
 
 	app.run(port=80, debug=True)
